bf665a794351bc360380df49a6b22b492ee0c1f9
ns336582 <69781177+ns336582@users.noreply.github.com>
2020-09-14 09:32:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 'use strict';
+// fs(File System) モジュールを読み込んで使えるようにする
 const fs = require('fs');
+// readline モジュールを読み込んで使えるようにする
 const readline = require('readline');
+//popu-pref.csv をファイルとして読み込める状態にする
 const rs = fs.createReadStream('./popu-pref.csv');
+// readline モジュールに rs を設定する
 const rl = readline.createInterface({ input: rs, output: {} });
+ // key: 都道府県 value: 集計データのオブジェクト
 const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
+// popu-pref.csv のデータを一行ずつ読み込んで、設定された関数を実行する
 rl.on('line', lineString => {
+  // ["2010", "北海道", "237155", "258530"]のようなデータ配列に分配
   const columns = lineString.split(',');
-  const year = parseInt(columns[0]);
-  const prefecture = columns[1];
-  const popu = parseInt(columns[3]);
+  const year = parseInt(columns[0]);　// 年
+  const prefecture = columns[1]; // 都道府県名
+  const popu = parseInt(columns[3]);// 15～19歳の人口
   if (year === 2010 || year === 2015) {
     let value = prefectureDataMap.get(prefecture);
     if (!value) {
